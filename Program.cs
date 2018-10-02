463bc1c21299dfb0a880a2930d165293f8e87362
BusinessChain@protonmail.com <BusinessChain@protonmail.com>
2018-10-02 21:36:19 +0200

    Outsourced Bitcoin node from form to program.cs

--- Program.cs
+++ Program.cs
@@ -4,16 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using BToken.Bitcoin;
+
 namespace BToken
 {
   static class Program
   {
+    public static BitcoinNode Node = new BitcoinNode();
+
     /// <summary>
     /// The main entry point for the application.
     /// </summary>
     [STAThread]
     static void Main()
     {
+      try
+      {
+        Task startNodeTask = Node.StartAsync();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(string.Format("Ups, something went wrong: '{0}'", ex.Message));
+      }
+
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new Form1());
