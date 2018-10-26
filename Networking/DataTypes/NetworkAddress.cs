290869cbe33de28278b37436d64081e9cbfbe574
BusinessChain@protonmail.com <BusinessChain@protonmail.com>
2018-10-26 17:04:55 +0200

    parse incomming network address messages, but does not use them yet.

---
+++ Networking/DataTypes/NetworkAddress.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BToken.Networking
+{
+  class NetworkAddress
+  {
+    public UInt32 UnixTimeSeconds { get; private set; }
+    public UInt64 NetworkServices { get; private set; }
+    public IPAddress IPAddress { get; private set; }
+    public UInt16 Port { get; private set; }
+
+
+    public NetworkAddress(
+      UInt32 unixTimeSeconds,
+      UInt64 networkServices,
+      IPAddress iPAddress,
+      UInt16 port)
+    {
+      UnixTimeSeconds = unixTimeSeconds;
+      NetworkServices = networkServices;
+      IPAddress = iPAddress;
+      Port = port;
+    }
+
+    public static NetworkAddress ParseAddress(byte[] buffer, ref int startIndex)
+    {
+      UInt32 unixTimeSeconds = BitConverter.ToUInt32(buffer, startIndex);
+      startIndex += 4;
+
+      UInt64 networkServices = BitConverter.ToUInt64(buffer, startIndex);
+      startIndex += 8;
+
+      IPAddress iPAddress = new IPAddress(buffer.Skip(startIndex).Take(16).ToArray());
+      startIndex += 16;
+
+      UInt16 port = BitConverter.ToUInt16(buffer, startIndex);
+      startIndex += 2;
+
+      return new NetworkAddress(
+        unixTimeSeconds,
+        networkServices,
+        iPAddress,
+        port);
+    }
+  }
+}
---
+++ Networking/Messages/AddressMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BToken.Networking
+{
+  partial class Network
+  {
+    class AddressMessage : NetworkMessage
+    {
+      public List<NetworkAddress> NetworkAddresses { get; private set; } = new List<NetworkAddress>();
+
+
+      public AddressMessage(NetworkMessage networkMessage) : base("addr", networkMessage.Payload)
+      {
+        DeserializePayload();
+      }
+      void DeserializePayload()
+      {
+        int startIndex = 0;
+
+        int addressesCount = (int)VarInt.GetUInt64(Payload, ref startIndex);
+        for (int i = 0; i < addressesCount; i++)
+        {
+          NetworkAddresses.Add(NetworkAddress.ParseAddress(Payload, ref startIndex));
+        }
+      }
+    }
+  }
+}
